e0efb780127bac0f529d7fff3232f5afb4b814ee
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2023-12-14 13:08:15 +0100

    release 1.0.0.0

--- src/main/java/de/medizininformatik_initiative/process/data_transfer/DataTransferProcessPluginDefinition.java
+++ src/main/java/de/medizininformatik_initiative/process/data_transfer/DataTransferProcessPluginDefinition.java
@@ -13,7 +13,7 @@ import dev.dsf.bpe.v1.ProcessPluginDefinition;
 public class DataTransferProcessPluginDefinition implements ProcessPluginDefinition
 {
 	public static final String VERSION = "1.0.0.0";
-	public static final LocalDate RELEASE_DATE = LocalDate.of(2022, 9, 13);
+	public static final LocalDate RELEASE_DATE = LocalDate.of(2023, 12, 14);
 
 	@Override
 	public String getName()
